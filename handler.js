440a13806af6fb2a93c381762391f0c959ba7818
faskan <faisalkhan@Faisals-MacBook-Pro.local>
2021-09-27 22:12:04 +0200

    first commit

---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const PDFKit = require("pdfkit")
+
+module.exports.generatePdf = async (event) => {
+  const text = event.queryStringParameters.text || 'Hello world'; 
+
+  return new Promise(resolve => {
+    const doc = new PDFKit()
+
+    doc.text(text)
+
+    const buffers = []
+    doc.on("data", buffers.push.bind(buffers))
+    doc.on("end", () => {
+      const pdf = Buffer.concat(buffers)
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/pdf",
+        },
+        body: pdf.toString("base64"),
+        isBase64Encoded: true,
+      }
+      resolve(response);
+    })
+
+    doc.end()
+  });
+}
